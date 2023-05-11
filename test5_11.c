f245c40c4975e6cf8d343293d95e95298f73d251
XUE——student <114730124+xuestudent@users.noreply.github.com>
2023-05-11 19:59:48 +0800

    5_11

---
+++ test5_11.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//显示浮点个数数字
+//int main()
+//{
+//    float a = 0;
+//    scanf("%f", &a);
+//    printf("%d", (int)a % 10);
+//
+//    return 0;
+//}
