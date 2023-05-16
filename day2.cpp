e368006e3d40eaa5c07172cddc6f604783787d8b
XUE——student <114730124+xuestudent@users.noreply.github.com>
2023-05-16 22:23:00 +0800

    day2
    day2

---
+++ day2.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//2的平方
+#include <stdio.h>
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int value = 1;
+//    while (n)
+//    {
+//        value = 2 * value;
+//        n--;
+//    }
+//    printf("%d", value);
+//    return 0;
+//}
+#include <stdio.h>
+
+int main()
+{
+    long long age = 0;
+    scanf("%d", &age);
+    printf("%lld", age * 31560000);
+    return 0;
+}
+///活多少岁
+#include <stdio.h>
+
+int main()
+{
+    long long n = 0;
+    scanf("%d", &n);
+    printf("%lld", n * 31560000);
+    return 0;
+}
