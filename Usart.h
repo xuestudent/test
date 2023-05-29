7b314f16af45b1b11679628312e67a81b3853ecb
XUE——student <114730124+xuestudent@users.noreply.github.com>
2023-05-29 21:29:07 +0800

    5_29_usat

---
+++ Usart.h
@@ -0,0 +1,6 @@
+#ifndef  __USART_H
+#define  __USART_H
+
+void UART_Init();
+void USART_SendByte(unsigned char Byte);
+#endif
---
+++ uart.c
@@ -0,0 +1,38 @@
+#include <REGX52.H>
+#include "Delay.h"
+#include "Usart.h"
+//void UART_Init()
+//{
+//	SCON=0x40;
+//	PCON &= 0x7F;
+//	TMOD &= 0x0F;			//设置定时器模式
+//	TMOD |= 0x20;			//设置定时器模式
+//  TL1 = 0xFA;			//设置定时初始值
+//	TH1 = 0xFA;			//设置定时重载值
+//	ET1 = 0;			//禁止定时器中断
+//	TR1 = 1;			//定时器1开始计时
+//}
+//void USART_SendByte(unsigned char Byte)//USART发送字符
+//{
+//	SBUF=Byte;//把数据写到SBUF寄存器
+//	while(TI==0);//直到发送控制器标志位置1（TI为1表示有数据）
+//	TI=0;//TI标志位，置1之后软件置0
+//}
+unsigned char date;
+void main()
+{
+
+   UART_Init();
+	
+    while(1)
+{
+	
+	USART_SendByte(date);
+	date++;
+	Delay(1000);
+}
+
+
+}
+
+
