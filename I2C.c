2fc5de0e97a7e51b096b8a3fe6614ae6fd4874af
XUE——student <114730124+xuestudent@users.noreply.github.com>
2023-05-31 19:21:48 +0800

    I2C.h

---
+++ I2C.c
@@ -0,0 +1,60 @@
+#include <REGX52.H>
+sbit I2C_SCL=P2^1;
+sbit I2C_SDA=P2^0;
+
+void I2C_Start(void)
+{
+	I2C_SDA=1;
+	I2C_SCL=1;
+	I2C_SDA=0;
+	I2C_SCL=0;	
+}
+void I2C_Stop(void)
+{
+	I2C_SDA=0;
+	I2C_SCL=1;
+	I2C_SDA=1;
+}
+void I2C_SendByte(unsigned char Byte)
+{
+	unsigned char i;
+	for(i=0;i<8;i++)
+	{
+	I2C_SDA=Byte&(0x80>>i);//取字节的最高位
+	I2C_SCL=1;
+	I2C_SCL=0;	
+	}
+}
+
+
+unsigned char I2C_ReceiveByte(void)
+{
+	unsigned char i,Byte=0x00;
+	I2C_SDA=1;
+	for(i=0;i<8;i++)
+	{
+	I2C_SCL=1;
+	if(I2C_SDA){Byte|=0x80>>i;}
+	I2C_SCL=0;
+  }
+	return Byte;
+}
+void I2C_SendAck(unsigned char AckBit)
+{
+	I2C_SDA=AckBit;
+	I2C_SCL=1;
+	I2C_SCL=0;
+}
+unsigned char I2C_ReceiveAck(void)
+{
+	
+	unsigned char AckBit=0x00;
+	I2C_SDA=1;
+	I2C_SCL=1;
+	AckBit=I2C_SDA;
+	I2C_SCL=0;
+	
+	return AckBit;
+}
+
+
