10acf20ce76d5835ac0b865fff4ae9e5527ee24e
XUE——student <114730124+xuestudent@users.noreply.github.com>
2023-06-16 22:33:28 +0800

    6_16

---
+++ test6_16.py
@@ -0,0 +1,24 @@
+import math
+print("hehe")#打印
+i=6
+print(i)
+print("123"+"456")
+a="张三"
+print(a)
+b=9
+print(math.sqrt(b))#计算平方根
+print(len(a))
+print("heheheheh"[3])
+#空值类型
+l=None
+print(type(6))
+print(type(l))
+num=int(input("请输入数字"))
+o=num+10
+print(o)
+num2=int(input("请输入数字"))
+#判断语句
+if num2<50:
+    print("太小了")
+else:
+    print("大")
